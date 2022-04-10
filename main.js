e60d515e7feb0798a071ddf2ad02da6ed1f150b0
Linneymx <98829164+Linneymx@users.noreply.github.com>
2022-04-09 21:21:15 -0500

    tablita v2.5

--- main.js
+++ main.js
@@ -1,62 +1,62 @@
 window.addEventListener('load', () => {
-	const form = document.querySelector("#NT-form");
-	const input = document.querySelector("#NT-input");
-	const list_el = document.querySelector("#tareas");
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 
-		const tarea = input.value;
+		const task = input.value;
 
-		const tarea_el = document.createElement('div');
-		tarea_el.classList.add('task');
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
 
-		const tarea_content_el = document.createElement('div');
-		tarea_content_el.classList.add('content');
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
 
-		tarea_el.appendChild(tarea_content_el);
+		task_el.appendChild(task_content_el);
 
-		const tarea_input_el = document.createElement('input');
-		tarea_input_el.classList.add('text');
-		tarea_input_el.type = 'text';
-		tarea_input_el.value = task;
-		tarea_input_el.setAttribute('readonly', 'readonly');
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
 
-		tarea_content_el.appendChild(task_input_el);
+		task_content_el.appendChild(task_input_el);
 
-		const tarea_actions_el = document.createElement('div');
-		tarea_actions_el.classList.add('actions');
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
 		
-		const tarea_edit_el = document.createElement('button');
-		tarea_edit_el.classList.add('edit');
-		tarea_edit_el.innerText = 'Editar';
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
 
-		const tarea_delete_el = document.createElement('button');
-		tarea_delete_el.classList.add('Eliminar');
-		tarea_delete_el.innerText = 'Borrar';
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
 
-		tarea_actions_el.appendChild(task_edit_el);
-		tarea_actions_el.appendChild(task_delete_el);
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
 
-		tarea_el.appendChild(task_actions_el);
+		task_el.appendChild(task_actions_el);
 
-		tarea_el.appendChild(task_el);
+		list_el.appendChild(task_el);
 
 		input.value = '';
 
-		tarea_edit_el.addEventListener('click', (e) => {
-			if (tarea_edit_el.innerText.toLowerCase() == "edit") {
-				tarea_edit_el.innerText = "Guardar";
-				tarea_input_el.removeAttribute("readonly");
-				tarea_input_el.focus();
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
 			} else {
-				tarea_edit_el.innerText = "Editar";
-				tarea_input_el.setAttribute("readonly", "readonly");
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
 			}
 		});
 
-		tarea_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(tarea_el);
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
 		});
 	});
 });
